ff17e847f01f70f34426aecd3a63377c54ef8513
ivanilic2636 <ivan.ilic028@gmail.com>
2017-05-19 02:18:07 +0200

    poruka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  tittle = "Log in";
+  user = "Your username and password ";
 }
